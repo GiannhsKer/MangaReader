7687390df83e34f1185573be9f722931f720a490
Giannhsker <giannishuin@gmail.com>
2020-03-13 23:16:31 +0200

    2

--- App.js
+++ App.js
@@ -1,38 +1,18 @@
 import * as React from 'react';
-import { List } from 'react-native-paper';
+import {Search} from './Components/Searchbar'
+import {Listt} from './Components/List'
 
-class MyComponent extends React.Component {
-    state = {
-        firstQuery: '',
-    };
+
+export default class App extends React.Component {
   
-    render() {
-        const { firstQuery } = this.state;
-        return (
-            <React.Fragment>
-                <Searchbar
-                        placeholder="Search"
-                        onChangeText={query => { this.setState({ firstQuery: query }); }}
-                        value={firstQuery}
-                    />
-                <List.Section>
-                <List.Subheader>Manga Reader</List.Subheader>
-                    <List.Item
-                        title="First Item"
-                        left={() => <List.Icon icon="folder" />}
-                    />
-                    <List.Item
-                        title="Second Item"
-                        left={() => <List.Icon color="#000" icon="folder" />}
-                    />
-                </List.Section>
-            </React.Fragment>
-            
-        );
-    }
+  render() {
+    return (
+        <React.Fragment>
+          <Search/>
+          <Listt/>
+        </React.Fragment>
+        
+    );
+  }
 }
-/*class MyComponent1 extends React.Component{
-            
-}
-*/
-export default MyComponent;
+
--- Components/Searchbar.js
+++ Components/Searchbar.js
@@ -1,28 +1,19 @@
-import React from 'react';
+import * as React from 'react';
 import { Searchbar } from 'react-native-paper';
-import { View } from 'react-native';
 
-export class Search extends React.Component {
+export default class MyComponent extends React.Component {
   state = {
     firstQuery: '',
   };
 
   render() {
     const { firstQuery } = this.state;
-    
     return (
-      <View>
-        <Searchbar >
-          value={firstQuery}   
-          onChangeText={query => { this.setState({ firstQuery: query }); }}
-          onIconPress={ () => this.computeGeoCoord()}
-        </Searchbar>
-        />
-      </View>
+      <Searchbar
+        placeholder="Search"
+        onChangeText={query => { this.setState({ firstQuery: query }); }}
+        value={firstQuery}
+      />
     );
   }
 }
-/*
-const styles = StyleSheet.create({
-  
-});*/
